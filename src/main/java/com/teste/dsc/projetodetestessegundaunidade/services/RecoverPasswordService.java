dc7ef0b2943569544daab2755a579a9f7abcc3f8
Neto-Pereira25 <devneto203@gmail.com>
2026-02-01 16:28:29 -0300

    test(tdd-pass): implement validation for password recovery with valid data

--- src/main/java/com/teste/dsc/projetodetestessegundaunidade/services/RecoverPasswordService.java
+++ src/main/java/com/teste/dsc/projetodetestessegundaunidade/services/RecoverPasswordService.java
@@ -1,6 +1,7 @@
 package com.teste.dsc.projetodetestessegundaunidade.services;
 
 import com.teste.dsc.projetodetestessegundaunidade.entities.User;
+import com.teste.dsc.projetodetestessegundaunidade.exceptions.BusinessRuleException;
 import com.teste.dsc.projetodetestessegundaunidade.repositories.UserRepository;
 
 public class RecoverPasswordService {
@@ -11,7 +12,24 @@ public class RecoverPasswordService {
     }
 
     public User recover(String email, String newPassword, String confirmNewPassword) {
+        
+        if (email == null || email.trim().isEmpty()) {
+            throw new BusinessRuleException("Email cannot be empty");
+        }
+
+        if (newPassword == null || newPassword.trim().isEmpty()) {
+            throw new BusinessRuleException("New password cannot be empty");
+        }
+
+        if (confirmNewPassword == null || confirmNewPassword.trim().isEmpty()) {
+            throw new BusinessRuleException("Confirm new password cannot be empty");
+        }
+        
         User user = userRepository.findByEmail(email);
+        
+        if (user == null) {
+            throw new BusinessRuleException("This email address is not registered.");
+        }
         
         user.setPassword(newPassword);
         user.setPasswordConfirmation(confirmNewPassword);
